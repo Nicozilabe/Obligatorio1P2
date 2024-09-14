cba8842f28170c58590fa011ea99d3364c39d1ab
Nicolás Ceglán <nicozilabe@hotmail.com>
2024-09-14 17:31:08 -0300

    Cambio contraseña furro generico

--- Entrega1/Program.cs
+++ Entrega1/Program.cs
@@ -6,7 +6,7 @@
         {
             Usuario u1 = new Usuario("Alberto","Nissman","anisman@ort.edu.uy","Anhero123");
             Cliente cliente = new Cliente("Sofía","eso","mylittlepony27@yahoo.com","Pinkypie7892",2020202);
-            Administrador admin = new Administrador("Furro", "1", "furrogenerico@aol.com", "ContraseniaFurraGenerica");
+            Administrador admin = new Administrador("Furro", "1", "furrogenerico@aol.com", "ContraseniaFurraGenerica2");
             Articulo a1 = new Articulo("Fur suit","Drogas duras",3499);
             Console.WriteLine(u1);
             Console.WriteLine(cliente);
