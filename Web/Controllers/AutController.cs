ea3507dbd024146aea8bb98dc0f91010899ffde9
AnisKiki <nadyrmiguel77@gmail.com>
2024-11-24 13:18:25 -0300

    Add Session program, IActionResult login.

---
+++ Web/Controllers/AutController.cs
@@ -0,0 +1,49 @@
+﻿using Entrega1;
+using Entrega1.Clases.Publicacion;
+using Entrega1.Clases.Usuarios;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class AutController : Controller
+    {
+        Sistema s = Sistema.GetInstancia();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel lm)
+        {
+            if (lm == null || string.IsNullOrEmpty(lm.Email) || string.IsNullOrEmpty(lm.Pass))
+            {
+
+            }
+            else
+            {
+                try
+                {
+                    Usuario usu = s.Login(lm.Email, lm.Pass);
+                    HttpContext.Session.SetInt32("logueadoId", usu.Id);
+                    HttpContext.Session.SetString("logueadoRol", usu.GetType().Name);
+                    HttpContext.Session.SetString("logueadoNombre", usu.Nombre);
+                    return RedirectToAction("Index", "Home");
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+                
+            }return View();
+        }
+    }
+}
---
+++ Web/Models/LoginModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Pass { get; set; }
+
+
+    }
+}
