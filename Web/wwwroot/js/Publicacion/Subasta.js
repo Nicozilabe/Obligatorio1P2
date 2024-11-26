11fb2d9d418be83faeb396020c1061b2cf7421ac
Nicozilabe <nicozilabe@hotmail.com>
2024-11-26 10:28:10 -0300

    Corrección js subasta

--- Web/wwwroot/js/Publicacion/Subasta.js
+++ Web/wwwroot/js/Publicacion/Subasta.js
@@ -1,13 +1,14 @@
-﻿document.querySelector("#formSubasta").addEventListener("submit", verificarForm())
+﻿document.querySelector("#formSubasta").addEventListener("submit", verificarForm)
 
 function verificarForm(evt) {
     evt.preventDefault()
-    m = dqs(montoSubasta).value
-    if (m == "" || m < 0 || m.isNaN()) {
+    m = dqs("montoSubasta").value
+        if (m == "" || m < 0 || isNaN(m)) {
         alert("Monto no válido")
     } else {
         this.submit()
     }
+
 }
 function dqs(id) {
     return document.querySelector("#"+id)
