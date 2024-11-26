c923158799af4302ac6aa182d87fc9d1c7575a1b
Nicozilabe <nicozilabe@hotmail.com>
2024-11-26 11:27:50 -0300

    Agregada verificación de estado venta en sistema y publicacioncontroller

--- Web/Controllers/PublicacionController.cs
+++ Web/Controllers/PublicacionController.cs
@@ -39,18 +39,24 @@ namespace Web.Controllers
         {
             if (HttpContext.Session.GetString("logueadoRol") == "Cliente")
             {
-                ViewBag.idCliente = HttpContext.Session.GetInt32("logueadoId");
-                //El metodo se encarga de registrar la compra, actualizar el estado de la publicacion y descontar el saldo del cliente. Luego se actualiza el saldo del cliente en la sesion LogueadoSoldo
-                try
+                if (s.GetVentaById(idVenta).Estado == TipoEstado.Abierta)
                 {
-                    s.CerrarPublicacion(idCliente, idVenta);
-                    Cliente c = s.GetCliente(HttpContext.Session.GetInt32("logueadoId"));
-                    HttpContext.Session.SetInt32("logueadoSaldo", (int)c.Saldo);
-                    ViewBag.msg = "Compra realizada efectivamente";
-                }
-                catch (Exception ex)
+                    ViewBag.idCliente = HttpContext.Session.GetInt32("logueadoId");
+                    //El metodo se encarga de registrar la compra, actualizar el estado de la publicacion y descontar el saldo del cliente. Luego se actualiza el saldo del cliente en la sesion LogueadoSoldo
+                    try
+                    {
+                        s.CerrarPublicacion(idCliente, idVenta);
+                        Cliente c = s.GetCliente(HttpContext.Session.GetInt32("logueadoId"));
+                        HttpContext.Session.SetInt32("logueadoSaldo", (int)c.Saldo);
+                        ViewBag.msg = "Compra realizada efectivamente";
+                    }
+                    catch (Exception ex)
+                    {
+                        ViewBag.msg = ex.Message;
+                    }
+                }else
                 {
-                    ViewBag.msg = ex.Message;
+                    ViewBag.msg = "Venta ya cerrada";
                 }
                 return View(s.GetVentaById(idVenta));
             }
